7cbe6b9bbcff2d1aa8b4e648f4b1d328e0a64fae
Axis-GLC <arnavreplituser@gmail.com>
2025-12-28 11:19:34 +0530

    Improve: expired demo link warning and improved setup.sh

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # whatsapp-chat-analysis
 A streamlit app to analyze your whatsapp chats
 
-Demo Link: https://wca-campusx.herokuapp.com/
+Demo Link: https://wca-campusx.herokuapp.com/ (Broken)
+
+**The demo link above does not work and users shall use another platform**
--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
+
 mkdir -p ~/.streamlit/
 
-echo "\
-[server]\n\
-port = $PORT\n\
-enableCORS = false\n\
-headless = true\n\
-\n\
-" > ~/.streamlit/config.toml
+cat > ~/.streamlit/config.toml <<EOF
+[server]
+port = $PORT
+enableCORS = false
+headless = true
+EOF
